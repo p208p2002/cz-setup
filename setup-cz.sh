f597575b3dcc82338a1094dee4269f5f5621c07c
p208p2002 <p208p2002@gmail.com>
2021-12-09 10:09:00 +0800

    Refactor: file rename

--- setup-cz.sh
+++
@@ -1,5 +0,0 @@
-npm install -g commitizen
-npm install -g cz-conventional-changelog
-npm install -g cz-customizable
-echo '{ "path": "cz-customizable" }' > ~/.czrc
-wget -O ~/.cz-config.js https://raw.githubusercontent.com/leoforfree/cz-customizable/master/cz-config-EXAMPLE.js
