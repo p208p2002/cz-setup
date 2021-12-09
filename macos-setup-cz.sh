920c8c8bf26e4511926ff3f17c6ae581969c772c
p208p2002 <p208p2002@gmail.com>
2021-12-09 10:09:56 +0800

    Feat: setup script for macos and ubuntu

---
+++ macos-setup-cz.sh
@@ -0,0 +1,1 @@
+curl -sf -L https://raw.githubusercontent.com/p208p2002/cz-setup/main/ubuntu-setup-cz.sh | sudo sh
---
+++ ubuntu-setup-cz.sh
@@ -0,0 +1,5 @@
+npm install -g commitizen
+npm install -g cz-conventional-changelog
+npm install -g cz-customizable
+echo '{ "path": "cz-customizable" }' > ~/.czrc
+wget -O ~/.cz-config.js https://raw.githubusercontent.com/p208p2002/cz-setup/main/.cz-config.js
